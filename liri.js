315f7dbc58a40d184f59c72fa8aea71d3823466d
fgibson94 <gibson.faithlee@yahoo.com>
2018-04-24 11:57:53 -0400

    cont

--- liri.js
+++ liri.js
@@ -1,1 +1,19 @@
+console.log("helloworld")
 require("dotenv").config();
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+`my-tweets`
+var Twitter = require('twitter');
+
+
+client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response) {
+    console.log(tweets);
+ });
+
+`spotify-this-song`
+
+`movie-this`
+
+`do-what-it-says`
